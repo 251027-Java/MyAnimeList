61d6e22dc81f713c42a292915782b410de9f5713
FahadNawazCs <fahadnawaz.cs99@gmail.com>
2025-11-20 23:46:14 -0600

    add logic to update watched status of an anime

--- src/main/java/com/animeapp/service/AnimeService.java
+++ src/main/java/com/animeapp/service/AnimeService.java
@@ -1,5 +1,8 @@
 package com.animeapp.service;
 
+import com.animeapp.model.UserAnimeWatched;
+import com.animeapp.model.requests.UserAnimeWatchedRequest;
+import com.animeapp.repository.UserAnimeWatchedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,15 +10,19 @@ import com.animeapp.model.Anime;
 import com.animeapp.repository.AnimeRepository;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class AnimeService {
     private final AnimeRepository animeRepository;
 
+    private final UserAnimeWatchedRepository userAnimeWatchedRepository;
+
     @Autowired
-    public AnimeService(AnimeRepository animeRepository) {
+    public AnimeService(AnimeRepository animeRepository, UserAnimeWatchedRepository userAnimeWatchedRepository) {
         this.animeRepository = animeRepository;
+        this.userAnimeWatchedRepository = userAnimeWatchedRepository;
     }
 
     public List<Anime> getAllAnime() {
@@ -26,8 +33,20 @@ public class AnimeService {
         return animeRepository.findByTitle(title);
     }
 
-//    public Anime updateRatings(String title, Integer rating){
-//
-//        return new Anime();
-//    }
+    public UserAnimeWatched updateAnimeWatchStatus(UserAnimeWatchedRequest request) {
+        UserAnimeWatched userAnimeWatched = new UserAnimeWatched(request.getUserId(), request.getAnimeId(), request.getWatched());
+        UserAnimeWatched exists = userAnimeWatchedRepository.findByUserIdAndAnimeId(userAnimeWatched.getUserId(), userAnimeWatched.getAnimeId());
+
+        //Save a record in the table if a user has not set an anime as watched or not watched
+        if (Objects.isNull(exists)) {
+            return userAnimeWatchedRepository.save(userAnimeWatched);
+        }
+        //if the user has previously set an anime as watched or not and wishes to change it, confirm that the watched
+        //statuses are different and update the same record.
+        else if (exists.getWatched() != request.getWatched()) {
+            exists.setWatched(request.getWatched());
+            return userAnimeWatchedRepository.save(exists);
+        }
+        return null;
+    }
 }
