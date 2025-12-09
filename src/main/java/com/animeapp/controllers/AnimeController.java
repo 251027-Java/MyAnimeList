e164615f56a934345c910b240987a662036c874d
snsorl <suryasolaiappan@gmail.com>
2025-12-09 17:26:23 -0600

    3 Java Files

--- src/main/java/com/animeapp/controllers/AnimeController.java
+++ src/main/java/com/animeapp/controllers/AnimeController.java
@@ -21,20 +21,24 @@ public class AnimeController {
     }
 
     @GetMapping()
-    public ResponseEntity<?> getAnimeByTitle(@RequestParam("title") String title){
+    public ResponseEntity<?> getAnimeByTitle(@RequestParam("title") String title) {
         Anime anime = animeService.getAnimeByTitle(title);
 
-        if(Objects.isNull(anime)){
+        if (Objects.isNull(anime)) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(anime);
     }
 
-    @PostMapping("/watch")
-    public ResponseEntity<?> updateAnimeWatchStatus(@RequestBody UserAnimeWatchedRequest request){
-        return  ResponseEntity.ok(animeService.updateAnimeWatchStatus(request));
+    // List all anime titles.
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllAnime() {
+        return ResponseEntity.ok(animeService.getAllAnime());
     }
 
-
+    @PostMapping("/watch")
+    public ResponseEntity<?> updateAnimeWatchStatus(@RequestBody UserAnimeWatchedRequest request) {
+        return ResponseEntity.ok(animeService.updateAnimeWatchStatus(request));
+    }
 
 }
