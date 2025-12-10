4633bff14ee7d8970feb6f86331b731c29e44f9e
snsorl <suryasolaiappan@gmail.com>
2025-12-09 18:55:06 -0600

    home page (needs --> search page)

--- MyAnimeListWebsite/src/app/Components/home-page/home-page.ts
+++ MyAnimeListWebsite/src/app/Components/home-page/home-page.ts
@@ -12,17 +12,42 @@ import { AnimeService, Anime } from '../../Service/anime-service';
 
 export class HomePage implements OnInit {
   animeList = signal<Anime[]>([]);
+  flippedCards = signal<Set<number>>(new Set());
+
   protected readonly title = signal('MyAnimeListWebsite');
   constructor(public animeService: AnimeService) { };
 
   ngOnInit() {
     this.animeService.getAllAnime().subscribe({
       next: (data) => {
-        this.animeList.set(data);
-        console.log('Data received:', data);
+        const filteredData = data
+          .filter(a => a.totalEpisodes > 0 && a.title !== '◯')
+          .sort((a, b) => a.title.localeCompare(b.title));
+
+        this.animeList.set(filteredData.slice(0, 216));
+gi
+        console.log('Original data length:', data.length);
+        console.log('Filtered data length:', filteredData.length);
+        console.log('Sliced data length:', this.animeList().length);
 
       },
       error: (err: any) => console.error('Error fetching anime:', err),
     });
+  }
+
+  toggleFlip(id: number) {
+    this.flippedCards.update((set) => {
+      const newSet = new Set(set);
+      if (newSet.has(id)) {
+        newSet.delete(id);
+      } else {
+        newSet.add(id);
+      }
+      return newSet;
+    });
+  }
+
+  isFlipped(id: number): boolean {
+    return this.flippedCards().has(id);
   }
 }
