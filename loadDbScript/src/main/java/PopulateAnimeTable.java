6c1a4b077f635cff698c2ef820e1100a8622b886
snsorl <suryasolaiappan@gmail.com>
2025-12-16 01:58:01 -0600

    Login.ts, My PopulateTable, Sign Up

--- loadDbScript/src/main/java/PopulateAnimeTable.java
+++ loadDbScript/src/main/java/PopulateAnimeTable.java
@@ -8,17 +8,20 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.Arrays;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URI;
 
 public class PopulateAnimeTable {
 
     public static void main(String[] args) {
-        String csvFile = "/Users/fahadnawaz/Documents/Revature/P0/loadDbScript/src/main/java/AnimeList.csv"; // path to your CSV file
+        String csvFile = "src/main/java/AnimeList.csv"; // path to your CSV file
 
-        String url = "jdbc:postgresql://localhost:5432/myanimelist";
+        String url = "jdbc:postgresql://localhost:5433/postgres";
         String user = "postgres";
         String password = "postgres";
 
-        String insertSQL = "INSERT INTO myanimelist.anime (title, total_episodes, status, avg_rating) VALUES (?, ?, ?, ?)";
+        String insertSQL = "INSERT INTO myanimelist.anime (title, total_episodes, status, avg_rating, image_url) VALUES (?, ?, ?, ?, ?)";
 
         try (
                 CSVReader reader = new CSVReader(new FileReader(csvFile));
@@ -49,15 +52,22 @@ public class PopulateAnimeTable {
                 if (status == null || status.isBlank()) {
                     pstmt.setNull(3, Types.VARCHAR);
                 } else {
-                    if(status.equals("Finished Airing")){
+                    if (status.equals("Finished Airing")) {
                         pstmt.setString(3, Status.COMPLETED.name());
-                    }
-                    else if(status.equals("Currently Airing")){
+                    } else if (status.equals("Currently Airing")) {
                         pstmt.setString(3, Status.ONGOING.name());
                     }
                 }
 
                 pstmt.setDouble(4, Math.round(((Math.random() * 9) + 1) * 100.0) / 100.0);
+
+                String imageUrl = getField(fields, 5);
+                if (imageUrl == null || imageUrl.isBlank()) {
+                    pstmt.setNull(5, Types.VARCHAR);
+                } else {
+                    String archivedUrl = "https://web.archive.org/web/20160826131916/" + imageUrl;
+                    pstmt.setString(5, archivedUrl);
+                }
 
                 pstmt.executeUpdate();
             }
